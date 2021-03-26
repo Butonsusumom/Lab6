fd9bb9f8354ae0721d192638116626850143cae8
Ksusha Tsubulko <43090851+Butonsusumom@users.noreply.github.com>
2021-03-26 20:46:29 +0100

    Initial commit

---
+++ app/src/main/java/com/tsybulko/lab6/MainActivity.java
@@ -0,0 +1,42 @@
+package com.tsybulko.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        if (savedInstanceState != null)
+            webView.restoreState(savedInstanceState);
+        else
+            webView.loadUrl("https://github.com/Butonsusumom");
+
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        webView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+
+    }
+
+}
